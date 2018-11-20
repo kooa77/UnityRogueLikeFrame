f5b97938e98bfae2a3d65e7db244ba5258beac24
kooa <kooa77@gmail.com>
2018-11-20 22:33:01 +0900

    적 여러명 생성

--- RogueLikeFrame/Assets/01. Scripts/MainGame/GameScene.cs
+++ RogueLikeFrame/Assets/01. Scripts/MainGame/GameScene.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class GameScene : MonoBehaviour
 {
+    [SerializeField] GameObject _enemyPrefab;
+
     [SerializeField] TileMap _tileMap;
     [SerializeField] Character _player;
-    [SerializeField] Character _soldier;
 
     // Unity
 
@@ -16,7 +17,18 @@ public class GameScene : MonoBehaviour
         GameManager.Instance.SetMap(_tileMap);
 
         _player.Init();
-        _soldier.Init();
+
+        int enemyCount = 10;
+        for(int i=0; i< enemyCount; i++)
+        {
+            GameObject enemyObject = GameObject.Instantiate(_enemyPrefab);
+            enemyObject.transform.SetParent(transform);
+            enemyObject.transform.localPosition = Vector3.zero;
+            enemyObject.transform.localScale = Vector3.one;
+
+            Character character = enemyObject.GetComponent<Character>();
+            character.Init();
+        }
 
         GameManager.Instance.SetPlayer(_player);
     }
